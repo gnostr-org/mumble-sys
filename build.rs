3f8083a29fcf7153d221216d9008f2b6d3086394
randymcmillan <randymcmillan@protonmail.com>
2025-11-17 13:20:06 -0500

    build.rs:ffi config

--- build.rs
+++ build.rs
@@ -5,49 +5,49 @@ use std::path::PathBuf;
 use std::process::Command;
 
 fn main() {
-    // Get Homebrew installation prefix for 'ice' package
-    let ice_prefix_output = Command::new("brew")
-        .arg("--prefix")
-        .arg("ice")
-        .output()
-        .expect("Failed to execute brew command for ice_prefix");
-    let ice_prefix = String::from_utf8(ice_prefix_output.stdout)
-        .expect("Failed to read stdout for ice_prefix")
-        .trim()
-        .to_string();
+    // // Get Homebrew installation prefix for 'ice' package
+    // let ice_prefix_output = Command::new("brew")
+    //     .arg("--prefix")
+    //     .arg("ice")
+    //     .output()
+    //     .expect("Failed to execute brew command for ice_prefix");
+    // let ice_prefix = String::from_utf8(ice_prefix_output.stdout)
+    //     .expect("Failed to read stdout for ice_prefix")
+    //     .trim()
+    //     .to_string();
 
-    if ice_prefix.is_empty() {
-        panic!("ERROR: Could not determine Homebrew prefix for 'ice'. Aborting.");
-    }
+    // if ice_prefix.is_empty() {
+    //     panic!("ERROR: Could not determine Homebrew prefix for 'ice'. Aborting.");
+    // }
 
-    let ice_cmake_dir = format!("{}/share/Ice/cmake", ice_prefix);
+    // let ice_cmake_dir = format!("{}/share/Ice/cmake", ice_prefix);
 
-    // Get installation prefix for MySQL client
-    let mysql_prefix_output = Command::new("brew")
-        .arg("--prefix")
-        .arg("mysql-client")
-        .output()
-        .expect("Failed to execute brew command for mysql_prefix");
-    let mysql_prefix = String::from_utf8(mysql_prefix_output.stdout)
-        .expect("Failed to read stdout for mysql_prefix")
-        .trim()
-        .to_string();
+    // // Get installation prefix for MySQL client
+    // let mysql_prefix_output = Command::new("brew")
+    //     .arg("--prefix")
+    //     .arg("mysql-client")
+    //     .output()
+    //     .expect("Failed to execute brew command for mysql_prefix");
+    // let mysql_prefix = String::from_utf8(mysql_prefix_output.stdout)
+    //     .expect("Failed to read stdout for mysql_prefix")
+    //     .trim()
+    //     .to_string();
 
-    if mysql_prefix.is_empty() {
-        panic!("ERROR: Could not determine Homebrew prefix for 'mysql-client'. Aborting.");
-    }
+    // if mysql_prefix.is_empty() {
+    //     panic!("ERROR: Could not determine Homebrew prefix for 'mysql-client'. Aborting.");
+    // }
 
     // build the mumble library
     let mut config = cmake::Config::new(".");
 
-    config.define("CMAKE_BUILD_TYPE", "Release");
-    config.define("Ice_DIR", ice_cmake_dir);
+    // config.define("CMAKE_BUILD_TYPE", "Release");
+    // config.define("Ice_DIR", ice_cmake_dir);
 
-    let cmake_prefix_path = format!("{};/opt/homebrew", mysql_prefix);
-    config.define("CMAKE_PREFIX_PATH", cmake_prefix_path);
-    config.define("MySQL_DIR", mysql_prefix.clone());
-    config.cxxflag(format!("-I{}/include", mysql_prefix));
-    config.cxxflag(format!("-L{}/lib", mysql_prefix));
+    // let cmake_prefix_path = format!("{}/opt/homebrew", mysql_prefix);
+    // config.define("CMAKE_PREFIX_PATH", cmake_prefix_path);
+    // config.define("MySQL_DIR", mysql_prefix.clone());
+    // config.cxxflag(format!("-I{}/include", mysql_prefix));
+    // config.cxxflag(format!("-L{}/lib", mysql_prefix));
 
     let dst = config.build();
 
@@ -70,8 +70,8 @@ fn main() {
     builder = builder.clang_arg("-I./plugins"); // Add mumble-sys/plugins
 
     // Add Homebrew and dependency include paths
-    builder = builder.clang_arg(format!("-I{}/include", ice_prefix));
-    builder = builder.clang_arg(format!("-I{}/include", mysql_prefix));
+    // builder = builder.clang_arg(format!("-I{}/include", ice_prefix));
+    // builder = builder.clang_arg(format!("-I{}/include", mysql_prefix));
 
     // Add Qt6 include paths based on qmake generated Makefile
     builder = builder.clang_arg("-I/opt/homebrew/Cellar/qt/6.9.3/lib/QtCore.framework/Headers");
@@ -88,10 +88,6 @@ fn main() {
         .blocklist_type("__gnu_cxx::.*")
         .blocklist_type("__.*")
         .default_non_copy_union_style(bindgen::NonCopyUnionStyle::ManuallyDrop);
-
-    builder.dump_preprocessed_input().expect("Failed to dump preprocessed input");
-
-    let bindings = builder
         .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
         .generate()
         .expect("Unable to generate bindings");
